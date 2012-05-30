603a852def950659a15c9a224c37e8aa5f848bfd
Nevin <niexinxin1986@gmail.com>
2012-05-30 22:24:57 +0800

    fix a bug in main.js

--- html/main.js
+++ html/main.js
@@ -1,5 +1,5 @@
 var getNetStateInfo = function() {
-    UVPlatform.exec(null, null, "NetState", "getStateInfo", ['paremet1', 'paremet2','测试3']);
+    PyGap.exec(null, null, "NetState", "getStateInfo", ['paremet1', 'paremet2','测试3']);
 };
 
 function init() {
